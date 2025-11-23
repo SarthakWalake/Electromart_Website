c32ff7092ff8cca106385e1fe7b9371021132ed1
Sarthak Walake <sarthakwalake7424@gmail.com>
2025-11-23 10:45:51 +0530

    BookingRepository

---
+++ electromartnew/electromartnew/api/src/main/java/com/electromart/api/repository/BookingRepository.java
@@ -0,0 +1,7 @@
+package com.electromart.api.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import com.electromart.api.model.Booking;
+public interface BookingRepository extends MongoRepository<Booking,String> {
+    
+}
