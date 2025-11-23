9d985c2edd986a6591f133c772d9cfa91b636b09
Sarthak Walake <sarthakwalake7424@gmail.com>
2025-11-23 10:48:31 +0530

    CartRepository

---
+++ electromartnew/electromartnew/api/src/main/java/com/electromart/api/repository/CartRepository.java
@@ -0,0 +1,9 @@
+package com.electromart.api.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.electromart.api.model.CartItem;
+
+public interface CartRepository extends MongoRepository<CartItem,String> {
+    
+}
