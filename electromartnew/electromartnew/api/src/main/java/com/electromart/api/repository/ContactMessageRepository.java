f6b13e3615aa28486f309b4cf69c34a9246b32dc
Sarthak Walake <sarthakwalake7424@gmail.com>
2025-11-23 10:51:32 +0530

    ContactMessageRepository

---
+++ electromartnew/electromartnew/api/src/main/java/com/electromart/api/repository/ContactMessageRepository.java
@@ -0,0 +1,9 @@
+package com.electromart.api.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.electromart.api.model.ContactMessage;
+
+public interface ContactMessageRepository extends MongoRepository<ContactMessage,String>{
+    
+}
